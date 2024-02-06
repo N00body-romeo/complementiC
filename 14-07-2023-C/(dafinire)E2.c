4e0f407745fdbcc51423d0cbf78479896c7bb31d
N00body-romeo <120739238+N00body-romeo@users.noreply.github.com>
2024-02-06 14:07:22 +0100

    Create (dafinire)E2.c

---
+++ 14-07-2023-C/(dafinire)E2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "aux.h"
+
+TipoLista append(TipoLista l, T e) { //per creare output e prefisso
+	if (estVuota(l)) return cons(e,l);
+  	return cons(car(l),append(cdr(l),e));
+}
+
+T trovaMax(TipoLista l, T max) { //serve per il prefisso
+	if (estVuota(l)) return max;
+  	if (max < car(l)) { max = car(l); }
+  	return trovaMax(cdr(l),max);
+}
+
+T somma(TipoLista l, T s) { //serve per la coda
+	if (estVuota(l)) return s;
+  	s+=car(l);
+  	return somma(cdr(l),s);
+}
+
+TipoLista aux(TipoLista l, TipoLista output, TipoLista precedenti) {
+	if (estVuota(l)) { return output; }
+  	T massimo_precedenti = trovaMax(precedenti,0);
+  	if (!estVuota(cdr(l))) {
+    	precedenti = append(precedenti,car(l));
+    }
+  	int media = 0;
+  	if (length(cdr(l)) != 0) {
+  		media = (somma(cdr(l),0))/length(cdr(l));
+    }
+  	output = append(output,massimo_precedenti-media);
+  	return aux(cdr(l),output,precedenti);
+}
+
+TipoLista mediaMenoMax(TipoLista l) {
+  	TipoLista precedenti = listaVuota();
+	TipoLista output = listaVuota();
+  	return aux(l,output,precedenti);
+}
+
+int* aggiungiINposizione(TipoLista l, int n, int* arr, int indice) {
+	if (estVuota(l)) return arr;
+  	if (indice == n) {
+    	arr[i] = car(l);
+    }
+  	return aggiungiINposizione(cdr(l),n,arr,indice++);
+}
